577981c55de6300136995ca9a0a6ccf45dc8d111
Ryoueye <43721600+Ryoueye@users.noreply.github.com>
2018-10-01 18:01:05 +0900

    dd

---
+++ Assets/Goal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+	public static bool goal;
+	void Start () {
+		goal = false;
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag == "Player"){
+			goal = true;
+	}
+}
+}
