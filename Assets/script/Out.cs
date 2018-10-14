58575cb206891922540a74b8cb30e8b7da2a8195
Ryoueye <43721600+Ryoueye@users.noreply.github.com>
2018-10-14 13:34:14 +0900

    2018.10.14

---
+++ Assets/script/Out.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Out : MonoBehaviour {
+	void OnTriggerEnter(Collider co1)
+	{
+		if (co1.gameObject.tag == "Player") {
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+
+}
---
+++ Assets/script/Timer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	public static float time;
+	void Start () {
+
+		time = 0;
+	}
+	void Update () {
+
+		if (Goal.goal == false) {
+			time += Time.deltaTime;
+		}
+		int t = Mathf.FloorToInt (time);
+		Text uiText = GetComponent<Text> ();
+		uiText.text = "Time : " + t.ToString ();
+	}
+}
