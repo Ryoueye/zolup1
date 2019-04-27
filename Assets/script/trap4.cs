942cc772525e822c9e9fb9234e983474d422aa27
Ryoueye <43721600+Ryoueye@users.noreply.github.com>
2019-04-27 17:32:15 +0900

    20190427

---
+++ Assets/script/trap4.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap4 : MonoBehaviour {
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.transform.position = new Vector3(-6+.35f, 7 ,-31.5f);
+        }
+    }
+}
