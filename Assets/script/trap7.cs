424d5cd527c6b999625ef0d5fc26e1fe6751e4e6
Ryoueye <43721600+Ryoueye@users.noreply.github.com>
2019-04-29 19:16:11 +0900

    2019.04.29

--- Assets/script/trap7.cs
+++ Assets/script/trap7.cs
@@ -2,11 +2,13 @@
 
 public class trap7 : MonoBehaviour {
     public GameObject obj;
+    public GameObject obj1;
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "trap7_tag")
         {
             Instantiate(obj, new Vector3(-66.75f, 0.5f, 11.33f), Quaternion.identity);
+            Destroy(obj1);
         }
     }
 }
---
+++ Assets/script/trap8.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap8 : MonoBehaviour {
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.transform.position = new Vector3(-67.14f, 8.16f ,-18.05f);
+        }
+    }
+}
