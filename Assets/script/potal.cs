a2eaddc1283b95fab8c8912b6624b4ed22c9cd51
Ryoueye <43721600+Ryoueye@users.noreply.github.com>
2019-05-01 05:29:35 +0900

    2019.05.01

---
+++ Assets/script/potal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potal : MonoBehaviour {
+
+    public float speed = 30.0f;
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/script/potal1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class potal1 : MonoBehaviour {
+
+    public float speed = 30.0f;
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
