55cd6d41c16e90fc5ffb60bfff2b270f6fd281c1
Ryoueye <43721600+Ryoueye@users.noreply.github.com>
2019-05-15 02:33:47 +0900

    2019.05.15

---
+++ Assets/Scenes/testscript/rigidbody.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigidbody : MonoBehaviour {
+    Rigidbody m_Rigidbody;
+    Vector3 m_ZAxis;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //This locks the RigidBody so that it does not move or rotate in the z axis (can be seen in Inspector).
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+        //Set up vector for moving the Rigidbody in the z axis
+        m_ZAxis = new Vector3(0, 0, 5);
+    }
+
+    void Update()
+    {
+        //Press space to remove the constraints on the RigidBody
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Remove all constraints
+            m_Rigidbody.constraints = RigidbodyConstraints.None;
+        }
+
+        //Press the right arrow key to move positively in the z axis if the constraints are removed
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //If the constraints are removed, the Rigidbody moves along the z axis
+            //If the constraints are there, no movement occurs
+            m_Rigidbody.velocity = m_ZAxis;
+        }
+
+        //Press the left arrow key to move negatively in the z axis if the constraints are removed
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            m_Rigidbody.velocity = -m_ZAxis;
+        }
+    }
+}
--- Assets/script/sound/crouch.cs
+++ Assets/script/sound/crouch.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class crouch : MonoBehaviour
 {
 //오디오 클립은 음악 소스
-    public AudioClip[] shotSound = new AudioClip[2];
+    public AudioClip[] shotSound = new AudioClip[3];
 
     //오디오 소스는 음악 재생기계 
     AudioSource Radio;
@@ -14,15 +14,30 @@ public class crouch : MonoBehaviour
     {
         //오디오소스 컴포넌트 받아주세요. 
         Radio = GetComponent<AudioSource>();
+       
     }
 
     void Update()
     {
         //콘솔 키 입력시 재생 
-        if (Input.GetKeyDown(KeyCode.E))
+        if(Input.GetKeyDown(KeyCode.E))
         {
-            //볼륨은 매니저로 전체 조절중입니다. 
+            if (Radio.isPlaying) return;
             Radio.PlayOneShot(shotSound[0]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (Radio.isPlaying) return;
+            Radio.loop = true;
+            Radio.PlayOneShot(shotSound[1]);
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Radio.isPlaying) return;
+            Radio.PlayOneShot(shotSound[2]);
         }
     }
 
