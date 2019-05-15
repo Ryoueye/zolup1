edfdd109ad4ca937e7f66cb1820e58791c2be1fc
Ryoueye <43721600+Ryoueye@users.noreply.github.com>
2019-05-15 21:12:25 +0900

    2019.05.15

---
+++ Assets/Scenes/testscript/rigidbodyoff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigidbodyoff : MonoBehaviour {
+    Rigidbody m_Rigidbody;
+
+    public GameObject obj;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            m_Rigidbody = GetComponent<Rigidbody>();
+            m_Rigidbody.constraints = RigidbodyConstraints.None;
+        }
+    }
+}
